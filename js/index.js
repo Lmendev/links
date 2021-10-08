59e1fd709fe8d69da2be45f3dd0f497c4397109a
Luis Mendoza <lmendoza.mg@gmail.com>
2021-10-08 20:09:42 +0200

    dark theme colors added

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 const toggleThemeSwitch = document.getElementById("toggle-theme-buttom")
 
-const toggleDarkTheme = () => {
+const toggleDarkTheme = (theme) => {
+    if (theme === 'dark'){
+        document.body.classList.add('dark-theme')
+        toggleThemeSwitch.classList.add('active')
+        return
+    }
+
     document.body.classList.toggle('dark-theme')
     toggleThemeSwitch.classList.toggle('active')
 }
 
-if (window.matchMedia?.('(prefers-color-scheme: dark)').matches)
-    toggleDarkTheme()
+if (window.matchMedia?.('(prefers-color-scheme: dark)').matches) 
+    toggleDarkTheme('dark')
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    const newColorScheme = e.matches ? "dark" : "light";
+    console.log(e)
+    const newColorSchemeIsDark = e.matches ? "dark" : "light";
+
+    if(newColorSchemeIsDark) 
+        toggleDarkTheme('dark')
 })
 
 toggleThemeSwitch.addEventListener('click', () => { toggleDarkTheme() })
